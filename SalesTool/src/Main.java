07b2bf21599087f2b96568d48553fd6a079cc3d3
Chape12 <chape12@student.sdu.dk>
2014-09-23 12:05:14 +0200

    Initial Checking of sales tool

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOW SALES DATA");
+	}
+
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,14 @@
+
+public class SalesData {
+
+	public SalesData() {
+		int data[] = {0, 4, 42};
+	}
+
+
+	public void display() {
+		System.out.println("TODO: Implement data display");
+
+	}
+
+}
